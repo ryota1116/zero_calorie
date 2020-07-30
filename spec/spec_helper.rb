dbb44fa014ef8fb8599d8ea442c5e56eff15dc3d
ryota1116 <ryota1116funa@gmail.com>
2020-07-31 00:25:31 +0900

    [Add]config/application.rbの設定を追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,19 +26,6 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-
-    # Rspec内でCapybara::DSLを使用出来るようにincludeする
-    # spec_helper内でincludeすると、実行時に以下のようにメッセージが表示される
-    # including Capybara::DSL in the global scope is not recommended!
-    config.include Capybara::DSL
-    # Rspec実行中にエラーが発生した時、実行終了後に赤色でエラー内容を表示する
-    config.failure_color = :red
-    # Rspec実行中にエラーが発生した時、停止せずに最後までスペックを実行する
-    config.fail_fast = false
-    # Rspecの実行結果を標準出力へカラーで出力する
-    config.color = true
-    # Rspecが実行結果を出力する時のフォーマットを指定して見やすくする (Default: :progress)
-    config.formatter = :documentation
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
