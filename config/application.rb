1dcd9eb440455feb83d43710f7e128012ab4b6f9
ryota1116 <ryota1116funa@gmail.com>
2020-07-31 00:25:31 +0900

    [Add]config/application.rbの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,25 @@ module ZeroCalorie
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # TimeZoneの設定
+    config.i18n.available_locales = %i[ja]
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # generateコマンド時に生成されるファイルに制限をかける
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: true,
+        fixture_replacement: :factory_bot,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,18 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  # https://qiita.com/necojackarc/items/8ba3a3fd2262f69adc34
+  # Rspec内でCapybara::DSLを使用出来るようにincludeする
+  # spec_helper内でincludeすると、実行時に以下のようにメッセージが表示される
+  # including Capybara::DSL in the global scope is not recommended!
+  config.include Capybara::DSL
+  # Rspec実行中にエラーが発生した時、実行終了後に赤色でエラー内容を表示する
+  config.failure_color = :red
+  # Rspec実行中にエラーが発生した時、停止せずに最後までスペックを実行する
+  config.fail_fast = false
+  # Rspecの実行結果を標準出力へカラーで出力する
+  config.color = true
+  # Rspecが実行結果を出力する時のフォーマットを指定して見やすくする (Default: :progress)
+  config.formatter = :documentation
 end
