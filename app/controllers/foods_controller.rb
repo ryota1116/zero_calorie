7caec6fc4c714ecb6a40f2a77cf77a1df3b73d19
ryota1116 <ryota1116funa@gmail.com>
2020-08-07 02:24:27 +0900

    [Fix]RESTを意識して画像検索機能を修正

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,47 @@
 class FoodsController < ApplicationController
   def create
   end
+
+  def index
+  end
+
+  def search_result
+    # リクエストの処理を書く
+
+    @meal_record = MealRecord.new(meal_record_params)
+    @meal_record.save
+
+    # クライアントを初期化
+    image_annotator = Google::Cloud::Vision.image_annotator
+
+    response = @meal_record.meal_picture.open do |file|
+
+      image_annotator.label_detection(
+          image: file,
+          max_results: 10
+      )
+    end
+
+    food_labels = []
+
+    # ラベル検出をリクエストしてレスポンスを処理する
+    response.responses.each do |res|
+      res.label_annotations.each { |label| food_labels << label.description }
+      # res.label_annotations.each do |label|
+      #   @labels << Food.search_by_label(label)
+      # end
+    end
+
+    @food_lists = []
+
+    food_labels.each do |food_label|
+      @food_lists = Food.search_by_label(food_label)
+    end
+  end
+
+  private
+
+  def meal_record_params
+    params.require(:meal_record).permit(:meal_picture)
+  end
 end
--- app/controllers/meal_records_controller.rb
+++ app/controllers/meal_records_controller.rb
@@ -9,51 +9,13 @@ class MealRecordsController < ApplicationController
   end
 
   def create
-    # リクエストの処理を書く
-
-    @meal_record = MealRecord.new(meal_record_params)
-    @meal_record.save
-
-    # クライアントを初期化
-    image_annotator = Google::Cloud::Vision.image_annotator
-
-    response = @meal_record.meal_picture.open do |file|
-
-      image_annotator.label_detection(
-          image: file,
-          max_results: 10
-      )
-    end
-
-    @json_string = []
-    # @labels = []
-
-    # ラベル検出をリクエストしてレスポンスを処理する
-    response.responses.each do |res|
-      res.label_annotations.each { |label| @json_string << label.description }
-      # res.label_annotations.each do |label|
-      #   puts label.description
-      #   @labels << Food.search_by_label(label)
-      # end
-    end
-
-    @food_lists = []
-    @json_string.each do |json|
-      @food_lists = Food.search_by_label(json)
-    end
-    # @food_lists = Food.where("name like a")
-
-    # @json_string.each do |label|
-    #   @labels << Food.search_by_label(label)
-    # end
-    # @food_lists = Food.search_by_label(@json_string)
   end
 
   def show
   end
 
   def index
-    @meal_record = MealRecord.all
+    # @meal_record = MealRecord.all
   end
 
   private
