e033db832efab08b2ad83b21882c4b5a2b8001be
ryota1116 <ryota1116funa@gmail.com>
2020-08-08 15:33:35 +0900

    [Add]MealRecordに外部キーfood_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_123950) do
+ActiveRecord::Schema.define(version: 2020_08_08_032158) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -46,7 +46,10 @@ ActiveRecord::Schema.define(version: 2020_08_04_123950) do
     t.datetime "meal_time", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "food_id", null: false
+    t.index ["food_id"], name: "index_meal_records_on_food_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "meal_records", "foods"
 end
