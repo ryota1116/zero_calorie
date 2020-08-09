68fd4e0c0da5ebfc32650ba3212817e30421c40c
ryota1116 <ryota1116funa@gmail.com>
2020-08-09 15:36:16 +0900

    [Add]HeadeとFooterを追加

--- db/migrate/20200802075924_create_meal_records.rb
+++ db/migrate/20200802075924_create_meal_records.rb
@@ -1,7 +1,7 @@
 class CreateMealRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :meal_records do |t|
-      t.datetime :meal_time, null:false
+      t.datetime :meal_time, null:false # DEFAULTで現在時刻にするか否か
 
       t.timestamps
     end
