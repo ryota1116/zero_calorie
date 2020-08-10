dc48c19c4a2106501ace1db5e55ecb64dc78fad7
ryota1116 <ryota1116funa@gmail.com>
2020-08-10 20:27:21 +0900

    [Add]Sorceryを導入

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  name             :string(255)
+#  salt             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  name             :string(255)
+#  salt             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
