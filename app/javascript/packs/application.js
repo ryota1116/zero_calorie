95622e9933bf073ffe1f2d35035d7c4fc0161617
ryota1116 <ryota1116funa@gmail.com>
2020-08-10 15:18:34 +0900

    [Add]BootStrapを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@ require("channels")
 require("jquery")
 require("jquery/submit_food_image")
 
+import 'bootstrap';
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,9 @@ const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
   })
 )
 
