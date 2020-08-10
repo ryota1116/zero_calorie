7b082e5bb2cfed46c6d3c7d2c2db561a659a9993
ryota1116 <ryota1116funa@gmail.com>
2020-08-11 00:07:33 +0900

    [Add]Userのモデルスペックを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,6 +16,9 @@
 #
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name) { |n| "user-#{n}" }
+    sequence(:email) { |n| "user-#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Userモデル" do
+    let(:user) { create(:user) }
+
+    it 'name, email, password, password_confirmationがあれば有効であること' do
+      expect(user).to be_valid
+    end
+    it 'nameが無ければ無効であること' do
+      user.name = nil
+      expect(user).to be_invalid
+    end
+    it 'emailが無ければ無効であること' do
+      user.email = nil
+      expect(user).to be_invalid
+    end
+    it 'emailが重複している場合、無効であること' do
+      other_user = build(:user, email: user.email)
+      expect(other_user).to be_invalid
+    end
+    it 'emailが重複していない場合、有効であること' do
+      other_user = build(:user, email: 'other@example.com')
+      expect(other_user).to be_valid
+    end
+    it 'passwordが無ければ無効であること' do
+      user = build(:user, password: nil)
+      expect(user).to be_invalid
+    end
+    it 'password_confirmationが無ければ無効であること' do
+      user = build(:user, password_confirmation: nil)
+      expect(user).to be_invalid
+    end
+  end
 end
