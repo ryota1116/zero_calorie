1f8217cd22a473a8d69ca766378264f12172d074
ryota1116 <ryota1116funa@gmail.com>
2020-08-10 17:22:56 +0900

    [Add]Semantic UIを導入

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'google-cloud-vision'
 gem "google-cloud-storage", "~> 1.8", require: false
 
 # UI/UX
+# semantic UI Framework
+gem 'semantic-ui-sass'
+# 国際化
 gem 'rails-i18n'
 # RailsでSEOの設定を行うGem
 # gem 'meta-tags'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,8 @@ const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
 )
