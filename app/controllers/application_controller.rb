0d0556190934b74243ff7f15b4d3fde492540492
ryota1116 <ryota1116funa@gmail.com>
2020-08-10 23:05:50 +0900

    [Fix]ユーザー周りのフォームを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
 end
