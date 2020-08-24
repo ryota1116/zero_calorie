71539ca250d5f882a92f64e71e54cdcc10e7416d
ryota1116 <ryota1116funa@gmail.com>
2020-08-24 12:41:47 +0900

    [Add]動的なpageタイトルの導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                   'Zero Calorie(管理画面)'
+                 else
+                   'Zero Calorie'
+                 end
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
