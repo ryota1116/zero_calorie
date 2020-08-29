e0816afcb2123b308dcf538563240563ef3199b0
ryota1116 <ryota1116funa@gmail.com>
2020-08-29 23:22:10 +0900

    [Add]Userにroleのenumを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,6 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  enum role: { general: 0, admin: 1 }
 end
