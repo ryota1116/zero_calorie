3bf0552a8288d743f71fb20539a0c6baaed3a35d
ryota1116 <ryota1116funa@gmail.com>
2020-08-31 23:42:10 +0900

    [Add]画像のプレビュー機能

---
+++ app/javascript/packs/meal_record_pictures_preview.js
@@ -0,0 +1,27 @@
+$(document).on("turbolinks:load", function () {
+  $("#meal_record_meal_record_pictures").on("change", function (e) {
+    // 画像の削除
+    $("#image_preview").empty();
+    var files = e.target.files;
+    var d = new $.Deferred().resolve();
+    $.each(files, function (i, file) {
+      d = d.then(function () {
+        return previewImage(file);
+      });
+    });
+  });
+
+  var previewImage = function (imageFile) {
+    var reader = new FileReader();
+    var img = new Image();
+    var def = $.Deferred();
+    reader.onload = function (e) {
+      // 画像の表示
+      $("#image_preview").append(img);
+      img.src = e.target.result;
+      def.resolve(img);
+    };
+    reader.readAsDataURL(imageFile);
+    return def.promise();
+  };
+});
