4aa483e3ffc16d60f2c6682029ef6ba903361e13
ryota1116 <ryota1116funa@gmail.com>
2020-09-16 00:49:56 +0900

    [Fix]seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require './db/seeds/user'
+Dir[File.expand_path('../seeds/food', __FILE__) << '/*.rb'].each do |file|
+  require file
+end
+require './db/seeds/meal_record'
 
-# be rails db:seed
-
-Food.create!([
-  {
-    name: "アイスコーヒー",
-    calorie: 0,
-    calorie_theory: "コーヒーを抽出している時、ゼロカロリーの成分だけが抽出されている。"
-  },
-  {
-    name: "白ごはん",
-    calorie: 0,
-    calorie_theory: "白い食べ物はカロリーが白紙に戻るからカロリーゼロ。"
-  }
-])
+# Food.create!([
+#   {
+#     name: "アイスコーヒー",
+#     calorie: 0,
+#     calorie_theory: "コーヒーを抽出している時、ゼロカロリーの成分だけが抽出されている。"
+#   },
+#   {
+#     name: "白ごはん",
+#     calorie: 0,
+#     calorie_theory: "白い食べ物はカロリーが白紙に戻るからカロリーゼロ。"
+#   }
+# ])
 
 # 3.times do |i|
 #   Article.create(user_id: User.find(i+1).id, title: "Star Wars #{i+1}")
