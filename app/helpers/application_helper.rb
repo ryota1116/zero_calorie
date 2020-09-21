37bf8b58b11e1675015da63ddf6b255aa87c049b
ryota1116 <ryota1116funa@gmail.com>
2020-09-21 13:43:29 +0900

    [Add]タイトルのテスト追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def page_title(page_title = '', admin = false)
     base_title = if admin
-                   'Zero Calorie(管理画面)'
+                   'Zerorie(管理画面)'
                  else
-                   'Zero Calorie'
+                   'Zerorie'
                  end
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
