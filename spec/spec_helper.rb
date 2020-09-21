04aaf286ada7b5a35ddbf084237a8cc77513e7dc
ryota1116 <ryota1116funa@gmail.com>
2020-09-21 12:35:01 +0900

    [Fix]rubocop修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ RSpec.configure do |config|
   require 'simplecov'
   # save to CircleCI's artifacts directory if we're on CircleCI
   if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
     SimpleCov.coverage_dir(dir)
   end
 
