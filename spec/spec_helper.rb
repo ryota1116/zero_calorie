e47b53eec222d2ed097632476e7d5de5793bfba8
ryota1116 <ryota1116funa@gmail.com>
2020-09-21 14:26:09 +0900

    [Add]Userテスト追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ RSpec.configure do |config|
   # codecov を実行するためのコード
   require 'simplecov'
   # save to CircleCI's artifacts directory if we're on CircleCI
-  if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-    SimpleCov.coverage_dir(dir)
-  end
+  # if ENV['CIRCLE_ARTIFACTS']
+  #   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  #   SimpleCov.coverage_dir(dir)
+  # end
 
   SimpleCov.start
 
