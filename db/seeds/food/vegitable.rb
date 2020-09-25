a5c3c2715cc9888d2a276e0c0450ae6711c5b15b
ryota1116 <ryota1116funa@gmail.com>
2020-09-25 20:44:06 +0900

    [fix]seedデータ

--- db/seeds/food/vegitable.rb
+++ db/seeds/food/vegitable.rb
@@ -298,9 +298,199 @@ Food.create!([
   }
 ])
 
-# FoodFoodGenre.create!([
-#   {
-#     food_genre_id: 1,
-#     food_id: 1
-#   },
-# ])
+FoodFoodGenre.create!([
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ナス").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "トマト").id
+  },
+
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ピーマン").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "パプリカ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "トウガラシ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "かぼちゃ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "きゅうり").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ゴーヤ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ズッキーニ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ヘチマ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "オクラ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "キャベツ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "シュンギク").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "セロリ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "高菜").id
+  },
+
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ちんげん菜").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "白菜").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "パセリ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ほうれん草").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "水菜").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "レタス").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "カブ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "三つ葉").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ルッコラ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "チシャ菜").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: " タマネギ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "にら").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ニンニク").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ネギ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ラッキョウ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "アスパラガス").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "たけのこ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "大根").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ゴボウ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ショウガ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "人参").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "蓮根").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ブロッコリー").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "カリフラワー").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "フキノトウ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "ミョウガ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "さつまいも").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "里芋").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "じゃがいも").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "長芋").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "山芋").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "モヤシ").id
+  },
+  {
+    food_genre_id: FoodGenre.find_by(genre_name: "野菜").id,
+    food_id: Food.find_by(name: "カイワレ大根").id
+  },
+])
