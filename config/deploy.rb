08dfcd8fd28788df18e5972e969857c5fbd62a91
ryota1116 <ryota1116funa@gmail.com>
2020-09-25 19:57:17 +0900

    [fix]deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,9 @@ set :user, 'ryota'
 # デプロイ先のディレクトリ
 set :deploy_to, "/var/www/zero_calorie"
 # linked_filesやlinked_dirsに設定したものに対してはシンボリックリンクが張られる
-set :linked_files, %w[config/master.key config/database.yml]
+set :linked_files, %w[config/credentials/production.key config/database.yml]
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets public/system vendor/bundle]
-append :linked_files, 'config/credentials/production.key'
+# append :linked_files, 'config/credentials/production.key'
 
 # rubyのバージョン
 set :rbenv_ruby, File.read('.ruby-version').strip
