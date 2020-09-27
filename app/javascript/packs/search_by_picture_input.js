230750f9ee9a7ba1130512d8874b74e30484febd
ryota1116 <ryota1116funa@gmail.com>
2020-09-27 16:52:45 +0900

    [add]検索フォーム修正

--- app/javascript/packs/search_by_picture_input.js
+++ app/javascript/packs/search_by_picture_input.js
@@ -1,8 +1,8 @@
-$(function () {
-  $("#search_by_picture form input[type=file]").on('change', function (e) {
-    console.log(e.target.files);
-    if (e.target.files[0].size > 0) {
-      $(this).parents('form').submit();
-    }
-  });
-});
+// $(function () {
+//   $("#search_by_picture form input[type=file]").on('change', function (e) {
+//     console.log(e.target.files);
+//     if (e.target.files[0].size > 0) {
+//       $(this).parents('form').submit();
+//     }
+//   });
+// });
