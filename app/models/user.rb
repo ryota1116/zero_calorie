973e093fed1866ef25c4ece1a170a606be2eafbf
ryota1116 <ryota1116funa@gmail.com>
2020-10-01 01:56:35 +0900

    [Fix]Userパスワード最低文字数変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: true }, presence: true
   validates :name, presence: true, length: { maximum: 255 }
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
