73123de60be8230b76240af04e586f555e740781
ryota1116 <ryota1116funa@gmail.com>
2020-10-30 14:35:59 +0900

    [Fix]rubocop対応

--- spec/decorators/meal_record_decorator_spec.rb
+++ spec/decorators/meal_record_decorator_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe MealRecordDecorator do
   let(:ice_cream) { create(:meal_record) }
-  let(:meal_record_with_food_genre) { create(:meal_record, :with_food_genre)}
+  let(:meal_record_with_food_genre) { create(:meal_record, :with_food_genre) }
 
   describe 'drawing_calorie_theory' do
     context 'Foodのカロリー理論がある場合' do
@@ -19,6 +19,7 @@ RSpec.describe MealRecordDecorator do
     context 'Foodのカロリー理論が無い場合' do
       context 'FoodGenreのカロリー理論がある場合' do
         before { ActiveDecorator::Decorator.instance.decorate meal_record_with_food_genre }
+
         it '1つ目のFoodGenreのカロリー理論を表示' do
           expect(meal_record_with_food_genre.drawing_calorie_theory).to eq meal_record_with_food_genre.food.food_genres[0].calorie_theory
         end
