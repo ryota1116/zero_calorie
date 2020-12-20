34d5b42f953a070617a752b795b1f66d4231c3b7
ryota1116 <ryota1116funa@gmail.com>
2020-12-20 18:24:56 +0900

    [Add]mkcertとngrokを導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # ホワイトリストに許可したいhostを追加
+  # config.hosts << '.ngrok.io'
+  # ホワイトリスト全体をクリア
+  config.hosts.clear
 end
